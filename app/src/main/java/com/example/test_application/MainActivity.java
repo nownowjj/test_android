25b25c9c0d43540fd85847e9454b20a1efde8fc4
jiwon.kim <jiwon.kim01@e4net.net>
2023-02-27 10:42:06 +0900

    where?

---
+++ app/src/main/java/com/example/test_application/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.test_application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.webkit.DownloadListener;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    private long backBtnTime = 0;
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.mainWebView);
+
+        webView.setWebViewClient(new WebViewClient());  // 새 창 띄우기 않기
+        webView.setWebChromeClient(new WebChromeClient());
+//        webView.setDownloadListener(new DownloadListener(){...});  // 파일 다운로드 설정
+
+        webView.getSettings().setLoadWithOverviewMode(true);  // WebView 화면크기에 맞추도록 설정 - setUseWideViewPort 와 같이 써야함
+        webView.getSettings().setUseWideViewPort(true);  // wide viewport 설정 - setLoadWithOverviewMode 와 같이 써야함
+
+        webView.getSettings().setSupportZoom(false);  // 줌 설정 여부
+        webView.getSettings().setBuiltInZoomControls(false);  // 줌 확대/축소 버튼 여부
+
+        webView.getSettings().setJavaScriptEnabled(true); // 자바스크립트 사용여부
+//        webview.addJavascriptInterface(new AndroidBridge(), "android");
+        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true); // javascript가 window.open()을 사용할 수 있도록 설정
+        webView.getSettings().setSupportMultipleWindows(true); // 멀티 윈도우 사용 여부
+
+        webView.getSettings().setDomStorageEnabled(true);  // 로컬 스토리지 (localStorage) 사용여부
+
+
+        //웹페이지 호출
+//        webView.loadUrl("http://www.naver.com");
+        webView.loadUrl("http://192.168.10.215:3000");
+//        webView.loadUrl("https://eunoia3jy.tistory.com");
+    }
+
+
+    // 뒤로가기 버튼 제어
+    @Override
+    public void onBackPressed() {
+        long curTime = System.currentTimeMillis();
+        long gapTime = curTime - backBtnTime;
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else if (0 <= gapTime && 2000 >= gapTime) {
+            super.onBackPressed();
+        } else {
+            backBtnTime = curTime;
+            Toast.makeText(this, "한번 더 누르면 종료됩니다.", Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+
+}
